adeef452a2c883579ce1502a01dbab90d0682c9b
David Sebastian Mendez <saurionx2005@gmail.com>
2025-09-10 10:12:59 -0300

    Creo CustomerController

---
+++ PWC.Challenge.Api/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PWC.Challenge.Api.Common.Controllers.Traditional;
+using PWC.Challenge.Application.Common;
+using PWC.Challenge.Application.Dtos;
+using PWC.Challenge.Domain.Entities;
+
+namespace PWC.Challenge.Api.Controllers
+{
+    [Route("customers")]
+    public class CustomersController:BaseWriteController<Customer,CustomerDto>
+    {
+        public CustomersController(ILogger<BaseWriteController<Customer, CustomerDto>> logger, IBaseService<Customer, CustomerDto> service) : base(logger, service)
+        {
+        }
+
+        [HttpGet("foo")]
+        public  async Task<IActionResult> GetDummy()
+        {
+            return Ok("foo");
+        }
+    }
+}
---
+++ PWC.Challenge.Application/Dtos/CustomerDto.cs
@@ -0,0 +1,12 @@
+﻿
+using PWC.Challenge.Application.Common.Entities;
+
+namespace PWC.Challenge.Application.Dtos;
+
+public class CustomerDto : EntityDto
+{
+    public string FullName { get;  set; }
+    public string Address { get;  set; }
+
+
+}
